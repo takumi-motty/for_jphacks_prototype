7c3554443b86d152c0bfb68dda837a134e0bdabe
Takumi Fujimoto <mottypublic@gmail.com>
2019-10-25 02:40:26 +0900

    プロトタイプ復旧

--- app/src/main/java/com/example/motty/forjphacksprototype/MainActivity.kt
+++ app/src/main/java/com/example/motty/forjphacksprototype/MainActivity.kt
@@ -7,13 +7,9 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() , SensorEventListener {
@@ -22,12 +18,6 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
     private var gManager: SensorManager by Delegates.notNull<SensorManager>()
     private var gSensor: Sensor by Delegates.notNull<Sensor>()
 
-    fun getToday(): String {
-        val date = Date()
-        val format = SimpleDateFormat("yyyy/MM/dd/HH:mm:ss", Locale.getDefault())
-        return format.format(date)
-    }
-
     private fun externalStoragePath(): String {
         return Environment.getExternalStorageDirectory().absolutePath
     }
@@ -35,6 +25,7 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
     override fun onSensorChanged(event: SensorEvent) {
         var strb: StringBuffer = StringBuffer()
         var strb2: StringBuffer = StringBuffer()
+
         var sensor: Sensor = event.sensor
 
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
@@ -45,6 +36,12 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
             strb.append("　z軸")
             strb.append(event.values[2])
             accelText.setText(strb.toString())
+            if (event.values[0] >= 9) gravityText.setText("←")
+            if (event.values[0] <= -9) gravityText.setText("→")
+            if (event.values[1] >= 9) gravityText.setText("↓")
+            if (event.values[1] <= -9) gravityText.setText("↑")
+            if (event.values[2] >= 9) gravityText.setText("下")
+            if (event.values[2] <= -9) gravityText.setText("上")
         } else if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
             strb2.append("x軸")
             strb2.append(event.values[0])
@@ -60,70 +57,70 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("path:", externalStoragePath())
-        Log.d("Multi", " getFilesDir(): " + getFilesDir())
-
-        val path = getFilesDir()
-
-        val fileName = "test.csv"
-        val file = File("/data/user/0/com.example.motty.getlabeltimeapplication/files", fileName)
-
         //センサーマネージャーを取得する
         mManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         //加速度計のセンサーを取得する
-        //その他のセンサーを取得する場合には引数を違うものに変更する
         mSensor = mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
         //ジャイロセンサ
         gManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         gSensor = gManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
 
 
-        boardButton.setOnClickListener {
-            timeText.setText(getToday())
-            labelsText.setText(getString(R.string.board))
-            var text = getString(R.string.board)+ ","+getToday()
-            file.writeText(text)
-        }
-        sitdownButton.setOnClickListener {
-            timeText.setText(getToday())
-            labelsText.setText(getString(R.string.sitdown))
-            var text = getString(R.string.sitdown)+ ","+getToday()
-            file.writeText(text)
-        }
-
-        getoffButton.setOnClickListener {
-            timeText.setText(getToday())
-            labelsText.setText(getString(R.string.getoff))
-            var text = getString(R.string.getoff)+ ","+getToday()
-            file.writeText(text)
-        }
-
-        readButton.setOnClickListener {
-            if (file.exists()) {
-                val contents = file.bufferedReader().use(BufferedReader::readText)
-                Log.d("data", contents)
+        action1Button.setOnTouchListener { _, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // ボタンが押し込まれたとき
+                    mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI)
+                    gManager.registerListener(this, gSensor, SensorManager.SENSOR_DELAY_UI)
+                    labelsText.setText("押しているよ")
+                }
+                MotionEvent.ACTION_UP -> {
+                    // ボタンが離されたとき
+                    mManager.unregisterListener(this)
+                    gManager.unregisterListener(this)
+                    labelsText.setText("離したよ")
+                    gravityText.setText("重力リセット")
+                }
             }
+            false
+        }
+        action2Button.setOnTouchListener { _, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // ボタンが押し込まれたとき
+                    mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI)
+                    gManager.registerListener(this, gSensor, SensorManager.SENSOR_DELAY_UI)
+                    labelsText.setText("押しているよ")
+                }
+                MotionEvent.ACTION_UP -> {
+                    // ボタンが離されたとき
+                    mManager.unregisterListener(this)
+                    gManager.unregisterListener(this)
+                    labelsText.setText("離したよ")
+                    gravityText.setText("重力リセット")
+                }
+            }
+            false
+        }
+        action3Button.setOnTouchListener { _, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // ボタンが押し込まれたとき
+                    mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI)
+                    gManager.registerListener(this, gSensor, SensorManager.SENSOR_DELAY_UI)
+                    labelsText.setText("押しているよ")
+                }
+                MotionEvent.ACTION_UP -> {
+                    // ボタンが離されたとき
+                    mManager.unregisterListener(this)
+                    gManager.unregisterListener(this)
+                    labelsText.setText("離したよ")
+                    gravityText.setText("重力リセット")
+                }
+            }
+            false
         }
     }
-
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    }
-    //アクティビティが閉じられたときにリスナーを解除する
-    override fun onPause() {
-        super.onPause()
-        //リスナーを解除しないとバックグラウンドにいるとき常にコールバックされ続ける
-        mManager.unregisterListener(this)
-        gManager.unregisterListener(this)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        //リスナーとセンサーオブジェクトを渡す
-        //第一引数はインターフェースを継承したクラス、今回はthis
-        //第二引数は取得したセンサーオブジェクト
-        //第三引数は更新頻度 UIはUI表示向き、FASTはできるだけ早く、GAMEはゲーム向き
-        mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI)
-        gManager.registerListener(this, gSensor, SensorManager.SENSOR_DELAY_UI)
     }
 }
